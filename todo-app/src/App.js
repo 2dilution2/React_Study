bc503096f7bcd990ddb799f6745f2af6a42f1efd
2dilution2 <2dilution2@gmail.com>
2023-09-13 18:20:48 +0900

    React setting 및 컴포넌트 생성

---
+++ todo-app/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {Component} from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent/>
+      <ForthComponent/>
+    </div>
+  );
+}
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent">First Component</div>
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className="SecondComponent">Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component {
+  render() {
+    return (
+      <div className="ThirdComponent">Third Component</div>
+    )
+  }
+}
+
+class ForthComponent extends Component {
+  render() {
+    return (
+      <div className="ForthComponent">Forth Component</div>
+    )
+  }
+}
+
+
+
+export default App;
